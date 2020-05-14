889d8f349be4b6711848da4d9fae63d9d8655687
wbekker <31740134+TapTap21@users.noreply.github.com>
2020-05-14 09:53:12 +0200

    CHANGE test variable name

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
-#export "AWS_SECRETS=$(cat secrets.json)"
+#export "SECRETS=$(cat secrets.json)"
 
-values=$(printenv AWS_SECRETS)
+values=$(printenv SECRETS)
 $( echo "$values" | jq -r 'keys[] as $k | "export \($k)=\(.[$k])"' )
 
 #printenv | grep MYSQL
